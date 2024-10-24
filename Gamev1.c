49e9afb463149f1dbc4dd744be136d24f24c8c7f
glebag <fglobsal@gmail.com>
2024-10-24 11:54:11 -0400

    Add more files

---
+++ Gamev1.c
@@ -0,0 +1,57 @@
+#include <ncurses.h>
+
+int main()
+{
+    int key = 0, y = 15, x = 15;
+    int max_y, max_x;
+
+    initscr();
+    keypad(stdscr, TRUE);
+    noecho();
+    curs_set(0);
+    getmaxyx(stdscr, max_y, max_x);
+
+    move(25, 25);
+    printw("BURGER");
+    refresh();
+
+    while (key != 'q')
+    {
+        move(0, 0);
+        printw("Get Gambash To The Burger Or Exit by Pressing Q");
+
+        key = getch();
+
+        move(y, x);
+        printw("       ");
+        
+
+        if (key == KEY_LEFT) {
+            if (x > 0) {
+                x--;
+            }
+        } else if (key == KEY_RIGHT) {
+            if (x < max_x - 1) {
+                x++;
+            }
+        } else if (key == KEY_UP) {
+            if (y > 0) {
+                y--;
+            }
+        } else if (key == KEY_DOWN) {
+            if (y < max_y - 1) {
+                y++;
+            }
+        }
+
+        move(y, x);
+        printw("Gambash");
+        refresh();
+
+        s
+    }
+
+    endwin();
+    return 0;
+}
+
